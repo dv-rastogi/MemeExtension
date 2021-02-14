14c7739d957bf78b3302981877606bffbf4e5fca
jishnu19048 <jishnu19048@iiitd.ac.in>
2021-02-14 18:17:31 +0530

    sound check

--- meme-extension/dist/getWord.js
+++ meme-extension/dist/getWord.js
@@ -4,6 +4,7 @@ function myNewFunction() {
     //alert(text);
     return text;
   }
+
 function httpGet(theUrl)
   {
       if (window.XMLHttpRequest)
@@ -18,13 +19,39 @@ function httpGet(theUrl)
       {
           if (xmlhttp.readyState==4 && xmlhttp.status==200)
           {
-              alert(xmlhttp.responseText);
-              console.log(xmlhttp.responseText);
-              return xmlhttp.responseText;
+                var modal = document.getElementById("myModal");
+                var img = document.getElementById("myImg");
+                var modalImg = document.getElementById("img01");
+                var captionText = document.getElementById("caption"); 
+                alert(xmlhttp.responseText);
+                console.log(xmlhttp.responseText);
+                return xmlhttp.responseText;
+          }
+      }
+      xmlhttp.open("GET", theUrl, false );
+      xmlhttp.send();    
+  }
+  function httpGetSound(theUrl)
+  {
+      if (window.XMLHttpRequest)
+      {// code for IE7+, Firefox, Chrome, Opera, Safari
+          xmlhttp=new XMLHttpRequest();
+      }
+      else
+      {// code for IE6, IE5
+          xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+      }
+      xmlhttp.onreadystatechange=function()
+      {
+          if (xmlhttp.readyState==4 && xmlhttp.status==200)
+          {
+            var audio =new Audio("sound.wav");
+            audio.play();
           }
       }
       xmlhttp.open("GET", theUrl, false );
       xmlhttp.send();    
   }
 var t=myNewFunction();
-httpGet('http://127.0.0.1:5000/getUrban/definition/'+t);
+httpGet('http://127.0.0.1:5000/getUrban/definition/'+t)
+httpGetSound('http://127.0.0.1:5000/getUrban/sound/'+t);
