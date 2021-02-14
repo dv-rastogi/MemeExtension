fb702851981437e925931ee7d0432a6c450b2310
watch24hrs-iiitd <divyansh19464@iiitd.ac.in>
2021-02-14 12:19:34 +0530

    Formed & Added services for urban dictionary

--- Readme.md
+++ Readme.md
@@ -7,6 +7,6 @@
 - Divyansh Rastogi
 
 ## To Run Backend Services
-- Open shell and navigate to the repository
+- Open shell and navigate to: `MemeExtension / services`
 - Run `pip3 install -r requirements.txt` &nbsp; (In case requirements are not installed / also can be done on a virtual env)
 - Run `python3 main.py`
--- services/main.py
+++ services/main.py
@@ -8,11 +8,6 @@ from webdriver_manager.chrome import ChromeDriverManager
 import json
 from flask import Flask
 
-BASE_URL = 'https://memes.com/search/?term='
-MEME_LIST_CLASS = 'pt-5 js-post-tiles-container js-search-feed d-flex flex-wrap justify-content-between post-tile-row'
-BASE_MEME_URL = "https://memes.com"
-MEME_CLASS = 'post-media-container'
-UNABLE_TO_LOAD = 'https://i.kym-cdn.com/photos/images/newsfeed/001/668/803/f75.jpg'
 
 application = Flask(__name__)
 
@@ -23,12 +18,19 @@ def index():
 
 
 @application.route('/getRandomMeme')
-def get_random_meme():
+def get_random_meme() -> str:
+    """
+    :return: URL to image
+    """
+    print("get_random_meme() requested")
+
+    CONNECTION_ERROR = "https://memegenerator.net/img/instances/65524138/connection-error-i-feel-so-rejected.jpg"
     uri = "https://meme-api.herokuapp.com/gimme"
     try:
         u_response = requests.get(uri)
     except requests.ConnectionError:
-        return "Connection Error"
+        print("Connection error")
+        return CONNECTION_ERROR
     j_response = u_response.text
     data = json.loads(j_response)
     return data["url"]
@@ -37,10 +39,16 @@ def get_random_meme():
 @application.route('/getSpecificMeme/<keyword>')
 def get_specific_meme(keyword: str) -> str:
     """
-    :param depth: Recursion depth
     :param keyword: keyword to split
     :return: URL to image
     """
+    print("get_specific_meme() requested for", keyword)
+
+    BASE_URL = 'https://memes.com/search/?term='
+    MEME_LIST_CLASS = 'pt-5 js-post-tiles-container js-search-feed d-flex flex-wrap justify-content-between post-tile-row'
+    BASE_MEME_URL = "https://memes.com"
+    MEME_CLASS = 'post-media-container'
+    UNABLE_TO_LOAD = 'https://i.kym-cdn.com/photos/images/newsfeed/001/668/803/f75.jpg'
 
     init_url = BASE_URL + '+'.join(keyword.split())
     print('URL formed', init_url)
@@ -88,6 +96,44 @@ def get_specific_meme(keyword: str) -> str:
     meme_link = meme_link_holder['src']
     return meme_link
 
+
+@application.route('/getUrban/<choice>/<keyword>')
+def get_urban_definition(choice: str, keyword: str) -> str:
+    """
+    :param keyword: keyword to split
+    :return: Meaning text
+    """
+    print("get_urban_definition() requested for ", choice, keyword)
+
+    NOT_FOUND = 'https://i.imgflip.com/1k5gq1.jpg'
+    BASE_URBAN_URL = "https://mashape-community-urban-dictionary.p.rapidapi.com/define"
+
+    keyword = ' '.join(keyword.split())
+    
+    querystring = {"term": keyword}
+    headers = {
+        'x-rapidapi-key': "5ed327aae2msh24174280b4e39a5p10a7f4jsn69ad8ec56ff4",
+        'x-rapidapi-host': "mashape-community-urban-dictionary.p.rapidapi.com"
+    }
+    response = requests.request("GET", BASE_URBAN_URL, headers=headers, params=querystring)
+    definitions = json.loads(response.text)['list']
+
+    # If no definition found
+    if len(definitions) == 0:
+        return NOT_FOUND
+
+    req = None
+    if choice == "definition":
+        req = definitions[0]['definition']
+    elif choice == "sound":
+        req_list = definitions[0]['sound_urls']
+        if len(req_list) == 0:
+            return "SOUND_NOT_FOUND"
+        else:
+            req = req_list[0]
+    else:
+        raise Exception("Invalid request")
+    return req
 
 if __name__ == "__main__":
     application.run()
