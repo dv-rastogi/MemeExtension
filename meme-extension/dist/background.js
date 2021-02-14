f7d1f0c6886de12f1a189d4ef2d0331aa29ea424
jishnu19048 <jishnu19048@iiitd.ac.in>
2021-02-14 14:24:32 +0530

    selection added

--- meme-extension/dist/background.js
+++ meme-extension/dist/background.js
@@ -12,3 +12,19 @@ contextMenus.createUDString =
             }
         }
     );
+
+chrome.contextMenus.onClicked.addListener(contextMenuHandler);
+
+function contextMenuHandler(info, tab){
+    if(info.menuItemId===contextMenus.createUDString){
+        chrome.tabs.executeScript(tab.ib, {
+
+            file: 'getWord.js'
+        }, _=>{
+            let e = chrome.runtime.lastError;
+            if(e !== undefined){
+                console.log(tab.ib, _, e);
+            }
+        });
+    }
+}
